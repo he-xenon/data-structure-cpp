598a8b0cc54e0312fb85bc48f330c42c47791d49
he-xenon <he_xenon@outlook.com>
2022-09-08 23:37:43 +0800

    Update mainDemo.cpp

--- src/mainDemo.cpp
+++ src/mainDemo.cpp
@@ -4,11 +4,40 @@ using namespace std;
 
 int main(int argc, char const **argv)
 {
+     //生成随机数组
     int MaxSize = 5;
     int MaxValue = 10000;
-    vector<int> arrTmp = generataRandomArray(MaxSize, MaxValue);
-    myprint(arrTmp);
+    vector<int> vecTmp = generataRandomArray(MaxSize, MaxValue);
+    myprint(vecTmp);
+    vector<int> vec1 = copyArray(vecTmp);
+    vector<int> vec2 = copyArray(vecTmp);
 
+    //系统排序算法
+    cout << "math sort algorithm start" << endl;
+    sort(vec1.begin(), vec1.end(),
+         [](int a, int b)
+         {
+             return a <= b ? true : false;
+         });
+    myprint(vec1);
+    cout << "math sort algorithm end" << endl;
+    
+    /*
+    //选择排序实现
+    selectionSort(vec2);
+    myprint(vec2);
+    
+    //测试
+    int tmp = isEqual(vec1, vec2);
+    if (tmp)
+    {
+        cout << "bubbleSort is Success" << endl;
+    }
+    else
+    {
+        cout << "bubbleSort is Fail" << endl;
+    }
+    */
     system("pause"); // 防止运行后自动退出，需头文件stdlib.h
     return 0;
 }
