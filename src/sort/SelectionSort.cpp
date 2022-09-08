e81a24c79b52f43de7861e20900b6bb072774724
he-xenon <he_xenon@outlook.com>
2022-09-08 23:35:40 +0800

    选择排序

--- src/sort/SelectionSort.cpp
+++ src/sort/SelectionSort.cpp
@@ -52,22 +52,38 @@ int selectionSort(int *arr, int size)
 
 int main(int argc, char const **argv)
 {
-    //数组
-    int arr[] = {10, 8, 9, 1};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    //生成随机数组
+    int MaxSize = 5;
+    int MaxValue = 10000;
+    vector<int> vecTmp = generataRandomArray(MaxSize, MaxValue);
+    myprint(vecTmp);
+    vector<int> vec1 = copyArray(vecTmp);
+    vector<int> vec2 = copyArray(vecTmp);
 
-    // vector
-    vector<int> vec;
-    for (size_t i = 0; i < size; i++)
+    //系统排序算法
+    cout << "math sort algorithm start" << endl;
+    sort(vec1.begin(), vec1.end(),
+         [](int a, int b)
+         {
+             return a <= b ? true : false;
+         });
+    myprint(vec1);
+    cout << "math sort algorithm end" << endl;
+
+    //选择排序实现
+    selectionSort(vec2);
+    myprint(vec2);
+    
+    //测试
+    int tmp = isEqual(vec1, vec2);
+    if (tmp)
     {
-        vec.push_back(arr[i]);
+        cout << "selectionSort is Success" << endl;
     }
-
-    selectionSort(arr, size);
-    myprint(arr, size);
-
-    selectionSort(vec);
-    myprint(vec); 
+    else
+    {
+        cout << "selectionSort is Fail" << endl;
+    }
 
     system("pause"); // 防止运行后自动退出，需头文件stdlib.h
     return 0;
